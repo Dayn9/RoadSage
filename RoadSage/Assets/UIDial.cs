8f69251951661dea252c33cc8003239d43818fc4
mal4739 <mal4739@IGM2025-06>
2018-12-05 18:54:53 -0500

    Added basic dial

---
+++ RoadSage/Assets/UIDial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIDial : MonoBehaviour, IDragHandler  {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RotateDialToCursor()
+    {
+        Vector2 direction = Input.mousePosition - GetComponent<RectTransform>().position;
+        GetComponent<Image>().fillAmount = (Vector2.SignedAngle(Vector2.down, direction) + 180)/360f;
+    }
+
+    public void OnDrag(PointerEventData p)
+    {
+        Vector2 direction = Input.mousePosition - GetComponent<RectTransform>().position;
+        GetComponent<Image>().fillAmount = (Vector2.SignedAngle(Vector2.down, direction) + 180) / 360f;
+        Debug.Log("waddup");
+    }
+}
